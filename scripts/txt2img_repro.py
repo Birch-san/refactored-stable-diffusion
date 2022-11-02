9edbac48b8677eea3df172af4b25ad706a83308a
Alex Birch <Birch-san@users.noreply.github.com>
2022-11-02 16:26:14 +0000

    simplify

--- scripts/txt2img_repro.py
+++ scripts/txt2img_repro.py
@@ -14,7 +14,6 @@ x = torch.full(shape, 7.0, device=device)
 
 target = torch.ones((1, 3, 128, 128), device=device)
 
-post_quant_conv = Conv2d(4, 4, 1, device=device)
 conv_in = Conv2d(4, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), device=device)
 conv_out = Conv2d(128, 3, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), device=device)
 norm = GroupNorm(32, 128, eps=1e-6, affine=True, device=device)
@@ -23,7 +22,6 @@ with enable_grad():#, autograd.detect_anomaly():
     x = x.detach().requires_grad_()
 
     denoised = 5.5 * x
-    denoised = post_quant_conv(denoised)
 
     decoded = conv_in(denoised)
     decoded = decoded+norm(decoded)
@@ -36,3 +34,5 @@ with enable_grad():#, autograd.detect_anomaly():
     loss: Tensor = (decoded - target).norm(dim=-1).sum()
     grad: Tensor = -autograd.grad(loss, x)[0]
     assert not grad.detach().isnan().any().item(), 'NaN gradients returned by autograd'
+
+print('Ran to completion successfully; did not encounter NaN gradients.')
